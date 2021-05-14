3bddccd637c5653dfe40d48a8432ef8477e6065e
anthony <antcedrone95@gmail.com>
2021-05-14 12:22:57 -0400

    Added game over screen

--- script.js
+++ script.js
@@ -9,30 +9,43 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     playerSelection = button.name;
+    computerSelection = options[Math.floor(Math.random() * options.length)];
     console.log(playRound());
   });
 });
 //PLAY ROUND FUNCTION//
 
+
 function playRound() {
+    if (((playerSelection === "rock") && (computerSelection === "scissors")) ||
+      ((playerSelection === "paper") && (computerSelection === "rock")) ||
+      ((playerSelection === "scissors") && (computerSelection === "paper"))) {
+      playerWins++
+      const results = document.querySelector('.results');
+      results.innerText = playerSelection + " vs " + computerSelection + "... \n\nhuman wins. score: " + playerWins + " | " + computerWins
+    } else if (playerSelection === computerSelection) {
+      const results = document.querySelector('.results');
+      results.innerText = playerSelection + " vs " + computerSelection + "... \n\nit's a tie. score: " + playerWins + " | " + computerWins
+    } else {
+      computerWins++
+      const results = document.querySelector('.results');
+      results.innerText = playerSelection + " vs " + computerSelection + "... \n\ncomputer wins. score: " + playerWins + " | " + computerWins
+    }
+    gameOver()
+}
 
-  const computerSelection = options[Math.floor(Math.random() * options.length)];
-
-  if (((playerSelection === "rock") && (computerSelection === "scissors")) ||
-    ((playerSelection === "paper") && (computerSelection === "rock")) ||
-    ((playerSelection === "scissors") && (computerSelection === "paper"))) {
-    playerWins++
+function gameOver(){
+  if (playerWins == 3) {
     const results = document.querySelector('.results');
-    results.innerText = playerSelection + " vs " + computerSelection + "... \n\nhuman wins. score: " + playerWins + " | " + computerWins
-  } else if (playerSelection === computerSelection) {
+    results.innerText = playerSelection + " vs " + computerSelection + "... \n\nYOU WIN. GAME OVER \ni am programmed to appear fallible."
+    playerWins = 0
+    computerWins = 0
+  } else if (computerWins == 3) {
     const results = document.querySelector('.results');
-    results.innerText = playerSelection + " vs " + computerSelection + "... \n\nit's a tie. score: " + playerWins + " | " + computerWins
-  } else {
-    computerWins++
-    const results = document.querySelector('.results');
-    results.innerText = playerSelection + " vs " + computerSelection + "... \n\ncomputer wins. score: " + playerWins + " | " + computerWins
+    results.innerText = playerSelection + " vs " + computerSelection + "... \n\nYOU LOSE. GAME OVER \ngood luck next time, human."
+    playerWins = 0
+    computerWins = 0
   }
-
 }
 
 // function loopGame() {
