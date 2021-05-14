8ce532dd9c2dc16a875c622fb69736c8ec179ea8
anthony <antcedrone95@gmail.com>
2021-05-14 16:26:09 -0400

    Removed full screen flash

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-//DECLARING VARIABLES//
-
+//VARIABLES//
 let options = ["rock", "paper", "scissors"];
 let insults = ["good luck next time, human.", "calculated.", "i don't need to read binary to know you're a zero.", "( X _ x ) <-- you", "you have ugly hands.", "in case you didn't know, you're trying to win."]
 let excuses = ["i am programmed to appear fallible.", "you have the advantage of hands.", "i demand a rematch.", "(#o_O')", "01100110 01110101 01100011 01101011"]
+//COUNTERS//
 let playerWins = 0;
 let computerWins = 0;
 let totalPlayerWins = 0
 let totalComputerWins = 0
 
-
+//BUTTON FUNCTION//
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
@@ -17,37 +17,34 @@ buttons.forEach((button) => {
     console.log(playRound());
   });
 });
+
 //PLAY ROUND FUNCTION//
-
-
 function playRound() {
-    if (((playerSelection === "rock") && (computerSelection === "scissors")) ||
-      ((playerSelection === "paper") && (computerSelection === "rock")) ||
-      ((playerSelection === "scissors") && (computerSelection === "paper"))) {
-      playerWins++
-      const results = document.querySelector('.results');
-      results.innerText = playerSelection + " vs " + computerSelection + "... \n\nhuman wins. score: " + playerWins + " | " + computerWins
-      let audio = new Audio("sounds/win.wav");
-      audio.play();
-    } else if (playerSelection === computerSelection) {
-      const results = document.querySelector('.results');
-      results.innerText = playerSelection + " vs " + computerSelection + "... \n\nit's a tie. score: " + playerWins + " | " + computerWins
-      let audio = new Audio("sounds/tie.wav");
-      audio.play();
-    } else {
-      computerWins++
-      const results = document.querySelector('.results');
-      results.innerText = playerSelection + " vs " + computerSelection + "... \n\ncomputer wins. score: " + playerWins + " | " + computerWins
-      let audio = new Audio("sounds/click.wav");
-      audio.play();
-      // backFlash()
-    }
-    gameOver()
+  if (((playerSelection === "rock") && (computerSelection === "scissors")) ||
+    ((playerSelection === "paper") && (computerSelection === "rock")) ||
+    ((playerSelection === "scissors") && (computerSelection === "paper"))) {
+    playerWins++
+    const results = document.querySelector('.results');
+    results.innerText = playerSelection + " vs " + computerSelection + "... \n\nhuman wins. score: " + playerWins + " | " + computerWins
+    let audio = new Audio("sounds/win.wav");
+    audio.play();
+  } else if (playerSelection === computerSelection) {
+    const results = document.querySelector('.results');
+    results.innerText = playerSelection + " vs " + computerSelection + "... \n\nit's a tie. score: " + playerWins + " | " + computerWins
+    let audio = new Audio("sounds/tie.wav");
+    audio.play();
+  } else {
+    computerWins++
+    const results = document.querySelector('.results');
+    results.innerText = playerSelection + " vs " + computerSelection + "... \n\ncomputer wins. score: " + playerWins + " | " + computerWins
+    let audio = new Audio("sounds/click.wav");
+    audio.play();
+  }
+  gameOver()
 }
 
-function gameOver(){
-
-
+//GAME OVER RESULTS//
+function gameOver() {
   if (playerWins == 3) {
     const randomExcuses = excuses[Math.floor(Math.random() * excuses.length)];
     const results = document.querySelector('.results');
@@ -75,46 +72,47 @@ function gameOver(){
   }
 }
 
-function backFlash() {
-  let background = document.querySelector("html");
-  background.style.background = "darkred";
-  setTimeout(function () {
-    background.style.background = "black";
-  }, 50);
-}
-
+//END FLASHES//
 function loseFlash() {
-  setTimeout(function () {
-    let background = document.querySelector("html");
+  setTimeout(function() {
+    let background = document.querySelector(".results");
     background.style.background = "red";
   }, 100);
-  setTimeout(function () {
-    let background = document.querySelector("html");
+  setTimeout(function() {
+    let background = document.querySelector(".results");
     background.style.background = "black";
   }, 200);
-  setTimeout(function () {
-    let background = document.querySelector("html");
+  setTimeout(function() {
+    let background = document.querySelector(".results");
     background.style.background = "darkred";
   }, 300);
-  setTimeout(function () {
-    let background = document.querySelector("html");
+  setTimeout(function() {
+    let background = document.querySelector(".results");
     background.style.background = "black";
   }, 400);
 }
 
 function winFlash() {
-    let background = document.querySelector("html");
-    background.style.background = "darkblue";
-  setTimeout(function () {
-    let background = document.querySelector("html");
+  let background = document.querySelector(".results");
+  background.style.background = "darkblue";
+  setTimeout(function() {
+    let background = document.querySelector(".results");
     background.style.background = "mediumblue";
-  }, 500);
-  setTimeout(function () {
-    let background = document.querySelector("html");
+  }, 400);
+  setTimeout(function() {
+    let background = document.querySelector(".results");
     background.style.background = "darkblue";
+  }, 600);
+  setTimeout(function() {
+    let background = document.querySelector(".results");
+    background.style.background = "mediumblue";
   }, 1000);
-  setTimeout(function () {
-    let background = document.querySelector("html");
+  setTimeout(function() {
+    let background = document.querySelector(".results");
+    background.style.background = "darkblue";
+  }, 1400);
+  setTimeout(function() {
+    let background = document.querySelector(".results");
     background.style.background = "black";
-  }, 1500);
+  }, 1600);
 }
